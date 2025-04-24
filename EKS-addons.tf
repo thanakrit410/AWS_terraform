95ea9ad599c0b4e1c5eca41089c2e4c102e1470e
thanakritPT <thanakrit.sa@pt.co.th>
2025-04-24 11:18:30 +0700

    commit

--- EKS-addons.tf
+++ EKS-addons.tf
@@ -1,10 +1,10 @@
 resource "aws_eks_addon" "coredns" {
   cluster_name  = "pc-eks"
   addon_name    = "coredns"
-  addon_version = "v1.11.1-eksbuild.11"  # อัปเดตเป็นเวอร์ชันที่รองรับ
+  addon_version = "v1.11.1-eksbuild.11"
 }
 
 resource "aws_eks_addon" "kubeproxy" {
-  cluster_name  = "pc-eks"
-  addon_name    = "kube-proxy"
+  cluster_name = "pc-eks"
+  addon_name   = "kube-proxy"
 }
